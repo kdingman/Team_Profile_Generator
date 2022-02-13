c2fd2bcf1c0eb05f6e98b9ad27c046ffffcc18d8
Kellly <kcdonlan@yahoo.com>
2022-02-13 11:19:37 -0500

    updated engineer tests

--- __tests__/engineer.test.js
+++ __tests__/engineer.test.js
@@ -2,27 +2,20 @@ const Engineer = require("../lib/engineer");
 
 jest.mock("../lib/engineer");
 
-    test("should generate engineer name", () => {
-        const engineer = new Engineer("Caleb");
-        expect(engineer.name).toBe("Caleb");
-    });
+test("should create an engineer object", () => {
+    const engineer = new Engineer("Caleb", 6, "kdingman", "caleb@yahoo.com");
 
-    test("should generate engineer ID number", ()=> {
-        const engineer = new Engineer("Caleb");
-        expext(engineer.id).toEqual(expect.any(Number));
-    });
+    expect(engineer.github).toEqual(expect.any(String))
+});
 
-    test("Should generate engineer GitHub account" , () => {
-        const engineer = new Engineer("Caleb");
-        expect(engineer.getGithub()).toEqual(expect.any(String));
-    });
+test(" should create engineer github value", () => {
+    const engineer = new Engineer("Caleb", 6, "kdingman", "caleb@yahoo.com");
 
-    test("should generate email address for Engineer", () => {
-        const engineer = new Engineer("Caleb");
-        expect(engineer.email).toEqual(expect.any(String));
-    })
+    expect(engineer.getGithub()).toEqual(expect.stringContaining(engineer.github.toString()));
+});
 
-    test("Should generate the employee details", () => {
-        const engineer = new Engineer("Caleb", 6, "gihub", "caleb@yahoo.com");
-        expect(engineer.getEmployeeType()).toEqual("Engineer");
-    });
+test("Should generate the employee details", () => {
+    const engineer = new Engineer("Caleb", 6, "kdingman", "caleb@yahoo.com");
+    
+    expect(engineer.getEmployeeType()).toEqual("Engineer");
+});
